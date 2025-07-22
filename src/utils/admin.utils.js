af5e8d01259cf4ac5cb98ffbffe8316170e5bedd
MawalFragrances <fragrances.mawal@gmail.com>
2025-07-22 18:51:47 +0500

    Resolve logout on refresh

--- src/utils/admin.utils.js
+++ src/utils/admin.utils.js
@@ -71,7 +71,7 @@ export const generateTokenAndSetCookie = async (res, userId) => {
     res.cookie("token", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production" ? true : false,
-        sameSite: process.env.NODE_ENV === "production" ? "strict" : "lax",
+        sameSite: "none",
         maxAge: 7 * 24 * 60 * 60 * 1000 // 7 DAYS
     })
 
